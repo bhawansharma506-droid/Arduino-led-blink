e5135e2879e0b998f3717a79e2717fc3ded44ca3
bhawan-1941 <bhawansharma1941@gmail.com>
2026-07-24 13:35:47 +0530

    go

---
+++ led-blink.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);// giving output as pin 13
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);//light up LED
+  delay(1000);// delay whole code and light up LED for 1 second
+  digitalWrite(13,LOW);//for turning off the LED
+  delay(1000)
+
+}
